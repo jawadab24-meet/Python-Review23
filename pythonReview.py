0cdef22736829ab95de84dcb64600201963e9fd7
jawadab24-meet <jawadabusalih521@gmail.com>
2023-07-17 00:11:04 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -1,21 +1,31 @@
 def create_youtube_video(title,description):
 	new_youtube_video={}
-	new_youtube_video["title"]=title:
-	new_youtube_video["description"= description]
+	new_youtube_video["title"]=title
+	new_youtube_video["description"]= description
 	new_youtube_video["likes"] = 0
-	new_youtube_video["dislike"]= 0
+	new_youtube_video["dislikes"]= 0
 	new_youtube_video["comments"]= {}
-	return create_youtube_video
-def like(create_youtube_video):
-	if like in create_youtube_video:
-		new_youtube_video["likes"] +=1
 	return new_youtube_video
-def dislike(create_youtube_video):
-	if dislike in create_youtube_video:
-		new_youtube_video["dislike"]+=1
+
+
+def like(new_youtube_video):
+	if "likes" in new_youtube_video:
+		new_youtube_video["likes"] += 1
 	return new_youtube_video
+
+def dislike(new_youtube_video):
+	if "dislikes" in new_youtube_video:
+		new_youtube_video["dislikes"]+=1
+	return new_youtube_video
+
 def add_comment(new_youtube_video,username,comment_text):
 	if "comments" in new_youtube_video:
 		new_youtube_video["comments"][username]= comment_text
 	return new_youtube_video
-new_youtube_video1 =create_youtube_video("title","description")
+
+video = create_youtube_video("Funny cat moments","very funny")
+for i in range(495):
+	like(video)
+
+video = add_comment(video, "User12", "LOL!")
+print(video)
